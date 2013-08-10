66b45f21716aba47a63d354170901bd0780cfcdc
Pete Schaffner <pjschaffner@gmail.com>
2013-08-10 11:27:34 -0400

    Initial commit

---
+++ init.js
@@ -0,0 +1,9 @@
+/**
+ * Balance command for HTML/CSS/JS
+ *
+ * Modeled after Espresso.app's Balance implementation
+ */
+
+Hooks.addMenuItem("Text/Select/Balance", "control-b", function () {
+  Alert.show('Balance', 'Balancing like a mofo', ['Sweet!']);
+});
